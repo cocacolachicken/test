999cb644cb56760e68ad85c87f5916be156a7b7e
tyler <tyler.gu@ashbury.ca>
2023-03-01 10:23:24 -0500

    Initial Commit

---
+++ test appppp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  test appppp
+//
+//  Created by Tyler Gu on 2023-03-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test appppp/test_apppppApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_apppppApp.swift
+//  test appppp
+//
+//  Created by Tyler Gu on 2023-03-01.
+//
+
+import SwiftUI
+
+@main
+struct test_apppppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
