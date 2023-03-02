076e89b09a6a8266bf5b1ddbec953dcf3dc75906
tyler <tyler.gu@ashbury.ca>
2023-03-02 14:19:17 -0500

    lll

--- test appppp/ContentView.swift
+++ test appppp/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
+            .navigationTitle("Menu")
         }
-        .padding()
     }
 }
 
